01d62534a9fc79e4979f58836c0fd4b6d6ba7698
高芹 <228506284@qq.com>
2019-12-18 16:20:51 +0800

    3

--- src/com/atguigu/test/Deme6.java
+++ src/com/atguigu/test/Deme6.java
@@ -1,7 +1,14 @@
-package com.atguigu.test;
+//package com.atguigu.test;
+
+import java.util.Arrays;
 
 public class Deme6 {
     public static void main(String[] args) {
+
+        int [] a={1,4,2};
+        Arrays.sort(a);
+        Arrays.toString(a);
+        System.out.println( Arrays.toString(a));
 
     }
 }
